e3975b4dbc765fdb2482904e4ec5312bcefa117b
mikekravets <michkravets@gmail.com>
2025-02-21 10:50:50 +0200

    Work on the function that builds the FIRST set

--- src/grammar.rs
+++ src/grammar.rs
@@ -98,6 +98,14 @@ where
             empty_token: LexicalToken::Term(empty_token),
         }
     }
+
+    fn terminals(&self) -> HashSet<&str> {
+        todo!()
+    }
+
+    fn reverse_dependencies(&self) -> HashMap<&str, Vec<&Production<T>>> {
+        todo!()
+    }
 }
 
 pub fn first<'grammar, T, U, F>(
@@ -106,31 +114,101 @@ pub fn first<'grammar, T, U, F>(
 where
     T: Terminal,
 {
+    let empty_lexeme = grammar.empty_token.lexeme();
     let mut map = HashMap::new();
-    let mut stack = Vec::<&Production<T>>::with_capacity(grammar.rules.len());
 
-    // TODO: retrieve terminal symbols from grammar and insert them into map
-    for rule in grammar.rules.iter() {
-        map.insert(rule.production.lhs.lexeme(), HashSet::new());
+    // Stack of productions to be processed by the algorithm
+    let mut stack = Vec::<&Production<T>>::with_capacity(grammar.rules.len());
+    let terminals = {
+        let mut terminals = HashSet::new();
+        for rule in grammar.rules.iter() {
+            for v in rule.production.rhs.iter() {
+                match v {
+                    LexicalToken::Term(t) => {
+                        terminals.insert(t.lexeme());
+                    }
+                    _ => (),
+                }
+            }
+        }
+
+        terminals
+    };
+
+    // Reverse dependencies are needed to correctly add productions to the stack of jobs.
+    // Key is lexeme, value is a vec of dependent productions
+    let mut rev_dependencies = HashMap::<&str, Vec<&Production<T>>>::new();
+
+    // Add terminals to first map
+    for term in terminals {
+        let mut set = HashSet::new();
+        set.insert(term);
+        map.insert(term, set);
     }
 
-    // key is lexeme, value is a vec of dependent productions
-    let mut rev_dependencies = HashMap::<&str, Vec<&Production<T>>>::new();
+    // Add non-terminals to first map.
+    // Add reverse dependencies for non-terminals.
+    // Add productions to the stack of jobs
+    for rule in grammar.rules.iter() {
+        map.insert(rule.production.lhs.lexeme(), HashSet::new());
+        rev_dependencies.insert(rule.production.lhs.lexeme(), Vec::default());
+        stack.push(&rule.production);
+    }
+
     for rule in grammar.rules.iter() {
         for token in rule.production.rhs.iter() {
-            rev_dependencies
-                .entry(token.lexeme())
-                .or_default()
-                .push(&rule.production);
+            match token {
+                LexicalToken::NonTerm(t) => {
+                    rev_dependencies
+                        .entry(t.lexeme)
+                        .or_default()
+                        .push(&rule.production);
+                }
+                _ => (),
+            }
         }
     }
 
     // println!("{:#?}", rev_dependencies);
-
-    // TODO: build first map
     while !stack.is_empty() {
         // SAFETY: non-emptiness of stack is ensured in the expression under while
         let p = stack.pop().unwrap();
+        let mut rhs_set = if let Some(s) = map.get(p.lhs.lexeme()) {
+            s.clone()
+        } else {
+            HashSet::new()
+        };
+        let mut trailing = false;
+
+        rhs_set.remove(empty_lexeme);
+        for t in p.rhs.iter().rev().skip(1).rev() {
+            if let Some(s) = map.get(t.lexeme()) {
+                if s.contains(empty_lexeme) {
+                    rhs_set.extend(s);
+                    rhs_set.remove(empty_lexeme);
+                } else {
+                    trailing = false;
+                    break;
+                }
+            }
+        }
+
+        if trailing
+            && map
+                .entry(p.rhs.last().unwrap().lexeme())
+                .or_insert(HashSet::new())
+                .contains(empty_lexeme)
+        {
+            rhs_set.insert(empty_lexeme);
+        }
+
+        let first_lhs = map.entry(p.lhs.lexeme()).or_insert(HashSet::new());
+        if first_lhs.difference(&rhs_set).count() > 0 {
+            // TODO: if we use `.or_default()` method anyway. Perhaps, there is no need to add lhs to rev_dependencies?
+            stack.extend_from_slice(rev_dependencies.entry(p.lhs.lexeme()).or_default());
+        }
+
+        first_lhs.extend(rhs_set);
     }
 
     map
